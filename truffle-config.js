add22725417c11e34d93d1404f18d02a53cfe406
Philippe <philippe@five.team>
2021-06-27 15:43:38 +0200

    Fichiers config

--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,8 @@
+
+const HDWalletProvider = require('@truffle/hdwallet-provider');
 const path = require("path");
+
+require('dotenv').config();
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -6,7 +10,35 @@ module.exports = {
   contracts_build_directory: path.join(__dirname, "client/src/contracts"),
   networks: {
     develop: {
-      port: 8545
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: 5777
+    },
+    mumbai: {
+      provider: () => new HDWalletProvider([ process.env.PRIVATE_KEY ], `https://polygon-mumbai.infura.io/v3/${process.env.INFURA_API_KEY}`),
+      network_id: 80001,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true
+    },
+    matic: {
+      provider: () => new HDWalletProvider([ process.env.PRIVATE_KEY ], `https://polygon-mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`),
+      network_id: 137,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true
+    },
+  },
+
+  compilers: {
+    solc: {
+      version: "0.8.4",
+      settings: {
+        optimizer: {
+          enabled: false,
+          runs: 200
+        },
+      }
     }
   }
 };
